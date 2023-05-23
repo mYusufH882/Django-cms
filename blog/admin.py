1ae6e0e863028930baeff8b1c6a70759f67c1c9d
YusufCoding <myusufh882@gmail.com>
2023-05-23 13:32:27 +0700

    Update code

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from .models import Blog
+from django.utils.html import mark_safe
 
 
 # Register your models here.
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ("judul", "kategori", "gambar", "tgl_publish", "tgl_dibuat")
+    list_display = ("judul", "kategori", "gambar_preview", "tgl_publish", "tgl_dibuat")
+    readonly_fields = ["gambar_preview"]
+
+    def gambar_preview(self, obj):
+        return mark_safe(
+            '<img src="{url}" width={width} height={height} />'.format(
+                url=obj.gambar_url, width=300, height=300
+            )
+        )
 
 
 admin.site.register(Blog, BlogAdmin)
